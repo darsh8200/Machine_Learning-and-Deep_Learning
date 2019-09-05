cc81b4e60220edea52537b739cf0ffee03c1d2ba
darsh8200 <46572301+darsh8200@users.noreply.github.com>
2019-09-05 14:40:01 +0530

    Update Linear_Regression.py

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -8,23 +8,7 @@ x_train = x_train.reshape(-1, 1)
 y_values = [i for i in range(11)]
 y_train = np.array(y_values, dtype = np.float32)
 y_train = y_train.reshape(-1, 1)
-class LinearRegressionModel(nn.Module):input_dim = 1
-output_dim = 1
-model = LinearRegressionModel(input_dim, output_dim)
-criterion = nn.MSELoss()
-learning_rate = 0.01
-optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
-epochs = 100
-for epoch in range(epochs):
-    epoch+=1
-    inputs = variable(torch.from_numpy(x_train))
-    labels = variable(torch.from_numpy(y_train))
-    optimizer.zero_grad()
-    outputs = model(inputs)
-    loss = criterion(outputs, labels)
-    loss.backward()
-    optimizer.step()
-    print("epoch{}, loss {} ".format(epoch, loss.data))
+class LinearRegressionModel(nn.Module):
     def __init__(self, input_size, output_size):
         super(LinearRegressionModel, self).__init__()
         self.linear = nn.Linear(input_dim, output_dim)
