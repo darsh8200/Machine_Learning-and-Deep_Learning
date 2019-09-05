d9b313bea154de88ec05409c9599af1366b57de8
darsh8200 <46572301+darsh8200@users.noreply.github.com>
2019-09-05 16:05:24 +0530

    Create Convolutional_Neural_Network(same_padding,avgpool).py

---
+++ Convolutional_Neural_Network(same_padding,avgpool).py
@@ -0,0 +1,100 @@
+import torch, numpy as np, torch.nn as nn
+from torch.autograd import variable
+import torchvision.transforms as transforms, torchvision.datasets as dsets
+
+train_dataset = dsets.MNIST(root = "./data", 
+                            train = True,
+                            transform = transforms.ToTensor(),
+                            download = True)
+
+test_dataset = dsets.MNIST(root = "./data", 
+                            train = False,
+                            transform = transforms.ToTensor())
+
+batch_size = 100
+n_iters = 3000
+num_epochs = n_iters / (len(train_dataset) / batch_size)
+num_epochs = int(num_epochs)
+
+train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
+                                       batch_size = batch_size,
+                                       shuffle = True)
+
+test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
+                                          batch_size = batch_size,
+                                          shuffle = False)
+
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+        self.cnn1 = nn.Conv2d(in_channels = 1, out_channels = 16, kernel_size = 5, stride = 1, padding = 2)
+        self.relu1 = nn.ReLU()
+        
+        self.avgpool1 = nn.AvgPool2d(kernel_size = 2)
+        
+        self.cnn2 = nn.Conv2d(in_channels = 16, out_channels = 32, kernel_size = 5, stride = 1, padding = 2)
+        self.relu2 = nn.ReLU()
+        
+        self.avgpool2 = nn.AvgPool2d(kernel_size = 2)
+        
+        self.fc1 = nn.Linear(32 * 7 * 7, 10)
+        
+    def forward(self, x):
+        out = self.cnn1(x)
+        out = self.relu1(out)
+        
+        out = self.avgpool1(out)
+        
+        out = self.cnn2(out)
+        out = self.relu2(out)
+        
+        out = self.avgpool2(out)
+        
+        #Resize
+        #original_size(100, 32, 7, 7)
+        #new_size(100, 32*7*7)
+        out = out.view(out.size(0), -1)
+        
+        out = self.fc1(out)
+        return out
+
+model = CNNModel()
+criterion = nn.CrossEntropyLoss()
+learning_rate = 0.01
+optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
+
+print(len(list(model.parameters())))
+print(list(model.parameters())[0].size())
+print(list(model.parameters())[1].size())
+print(list(model.parameters())[2].size())
+print(list(model.parameters())[3].size())
+print(list(model.parameters())[4].size())
+print(list(model.parameters())[5].size())
+
+iter = 0
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = variable(images)
+        labels = variable(labels)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        iter += 1
+
+        if iter % 500 == 0:
+            correct = 0
+            total = 0
+
+            for images, labels in test_loader:
+                images = variable(images)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct+= (predicted == labels).sum()
+
+            accuracy  =(100*correct)/total
+            
+            print("Iteration: {}. loss: {}. accuracy: {} ".format(iter, loss.data, accuracy))
